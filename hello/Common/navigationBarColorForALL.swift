2fd91f48fbefb18a5f334430ec0c19e2167782c8
xiaohutu <shanxi.1991@gmail.com>
2022-08-19 23:38:36 +0800

    调整 自定义导航栏样式

---
+++ hello/Common/navigationBarColorForALL.swift
@@ -0,0 +1,50 @@
+//
+//  NavigationBarStyle.swift
+//  HelloSwift
+//  修改App导航栏背景色，对所有页面生效
+//
+//  Created by 1 on 8/19/22.
+//
+
+import SwiftUI
+
+
+struct NavigationBarModifier: ViewModifier {
+
+    var backgroundColor: UIColor?
+    var titleColor: UIColor?
+
+    init(backgroundColor: UIColor?, titleColor: UIColor?) {
+        self.backgroundColor = backgroundColor
+        let coloredAppearance = UINavigationBarAppearance()
+        coloredAppearance.configureWithTransparentBackground()
+        coloredAppearance.backgroundColor = backgroundColor
+        coloredAppearance.titleTextAttributes = [.foregroundColor: titleColor ?? .white]
+        coloredAppearance.largeTitleTextAttributes = [.foregroundColor: titleColor ?? .white]
+
+        UINavigationBar.appearance().standardAppearance = coloredAppearance
+        UINavigationBar.appearance().compactAppearance = coloredAppearance
+        UINavigationBar.appearance().scrollEdgeAppearance = coloredAppearance
+    }
+
+    func body(content: Content) -> some View {
+        ZStack{
+            content
+            VStack {
+                GeometryReader { geometry in
+                    Color(self.backgroundColor ?? .clear)
+                        .frame(height: geometry.safeAreaInsets.top)
+                        .edgesIgnoringSafeArea(.top)
+                    Spacer()
+                }
+            }
+        }
+    }
+}
+
+extension View {
+    @ViewBuilder
+    func navigationBarColorForALL(backgroundColor: UIColor?, titleColor: UIColor?) -> some View {
+        self.modifier(NavigationBarModifier(backgroundColor: backgroundColor, titleColor: titleColor))
+    }
+}
--- hello/example/p_navigation.swift
+++ hello/example/p_navigation.swift
@@ -7,25 +7,101 @@
 
 import SwiftUI
 
+// 修改当前视图导航栏背景颜色
+struct NavigationBarStyle<S: ShapeStyle>: ViewModifier {
+    private var bgStyle: S
+    private var viewBackgroundColor: Color
+    
+    init(_ bgStyle: S, viewBackgroundColor: Color) {
+        self.bgStyle = bgStyle
+        self.viewBackgroundColor = viewBackgroundColor
+    }
+    
+    func body(content: Content) -> some View {
+        ZStack {
+            Color(UIColor.systemBackground)
+                .ignoresSafeArea(.all, edges: .bottom)
+            
+            content
+        }
+        .background(bgStyle)
+    }
+}
+
+extension View {
+    func navigationBarColorForCurrent<S: ShapeStyle>(_ bgStyle: S, viewBackgroundColor: Color = Color(UIColor.systemBackground)) -> some View {
+        modifier(NavigationBarStyle(bgStyle, viewBackgroundColor: viewBackgroundColor))
+    }
+}
+
 struct p_navigation: View {
+    
+    @State var isActive = false
+    @State var navBarBgColor: Color = .blue
+    @State var navBarFgColor: Color = .white
+    
+    // dismiss方法 适用于 iOS 15.0+
+    @Environment(\.dismiss) private var dismiss
+    
     var body: some View {
-        VStack {
-            Text("导航栏定制")
-                .font(.title)
-                .navigationBarItems(
-                    leading: Text(""),
-                    trailing:
-                    Button(action: {}) {
-                      Text("Save")
-                    }
-                )
+        VStack(alignment: .leading, spacing: 20) {
+            
+            Text("请注意，导航栏左边和右边按钮。")
+            
+            // 适用于13.0 ~ 16.0
+            if #unavailable(iOS 16.0) {
+                NavBar_13_16
+            } else {
+                NavBar_16
+            }
             
             Divider()
             
-            Text("注意：导航栏顶部右上角多了save")
+            Button("改变导航栏颜色为红色", action: {
+                self.isActive.toggle()
+                self.navBarBgColor = self.isActive ? .red : .blue
+            }).buttonStyle(PrimaryBtnStyle())
+            
         }
+        .padding()
+        .navigationBarColorForCurrent(navBarBgColor)
+        .navigationBarBackButtonHidden(true)
+        
+    }
+    
+    @available (iOS, introduced: 13.0, deprecated: 16.0, message: "此方法仅仅适用于13.0~16.0")
+    var NavBar_13_16: some View {
+        Text("")
+            .navigationBarItems(
+                leading: Button(action: {
+                    self.dismiss()
+                }) {
+                    Text("< 返回").foregroundColor(navBarFgColor)
+                },
+                trailing:
+                    Button(action: {}) {
+                        Text("分享").foregroundColor(navBarFgColor)
+                    }
+            )
+    }
+    
+    @available (iOS 16.0, *)
+    var NavBar_16: some View {
+        Text("")
+            .navigationBarItems(
+                leading: Button(action: {
+                    self.dismiss()
+                }) {
+                    Text("< 返回").foregroundColor(navBarFgColor)
+                },
+                trailing:
+                    Button(action: {}) {
+                        Text("分享").foregroundColor(navBarFgColor)
+                    }
+            )
     }
 }
+
 
 struct p_navigation_Previews: PreviewProvider {
     static var previews: some View {
