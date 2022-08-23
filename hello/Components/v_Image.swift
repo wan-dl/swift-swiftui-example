942a6741e924de68bea22b12ec51e6a1d24dbe8b
xiaohutu <shanxi.1991@gmail.com>
2022-08-23 23:19:41 +0800

    update image 网络图片请求

--- hello/Components/v_Image.swift
+++ hello/Components/v_Image.swift
@@ -2,7 +2,8 @@
 //  ViewImage.swift
 //  format
 //
-//  Created by hx on 7/29/22.
+//  Created by 1 on 7/29/22.
+//  update by 1 on 2022-08-23
 //
 
 import SwiftUI
@@ -12,7 +13,7 @@ struct v_Image: View {
     @State private var selectedTab: Int = 0
     
     var body: some View {
-        VStack(alignment: .leading) {
+        VStack(alignment: .center) {
             Picker("", selection: $selectedTab) {
                 Text("SF符号").tag(0)
                 Text("本地图片").tag(1)
@@ -20,57 +21,98 @@ struct v_Image: View {
                 Text("网络图片2").tag(3)
            }
            .pickerStyle(SegmentedPickerStyle())
-
+           
+           Spacer()
+            
            switch(selectedTab) {
            case 0: exampleSFImage()
            case 1: exampleLoadImage()
-           case 2: exampleNetWorkImage1()
-           case 3: exampleNetWorkImage2()
+           case 2: exampleNetWorkImageUseURLSession()
+           case 3: exampleNetWorkImageUseAsyncImage()
            default: exampleSFImage()}
             
-            Spacer()
+           Spacer()
         }
-        .navigationBarTitle(Text("Image"), displayMode: .inline)
+        .navigationTitle("Image")
+        .navigationBarTitleDisplayMode(.inline)
     }
 }
 
 struct exampleSFImage: View {
     var body: some View {
-        VStack(alignment: .center, spacing: 40) {
-            Image(systemName: "leaf.fill")
-                .foregroundColor(.green)
-        }
-        .padding()
+        Image(systemName: "leaf.fill")
+            .foregroundColor(.green)
     }
 }
 
 struct exampleLoadImage: View {
     var body: some View {
-        VStack() {
-            Image("wuhuan")
-                .resizable()
-                .aspectRatio(contentMode: .fit)
-                .scaledToFit()
-        }
+        Image("wuhuan")
+            .resizable()
+            .aspectRatio(contentMode: .fit)
+            .scaledToFit()
     }
 }
 
-struct exampleNetWorkImage1: View {
+struct exampleNetWorkImageUseURLSession: View {
     @State private var remoteImage: UIImage? = nil
-    let placholderImage = UIImage(named: "wuhuan")
+    @State var netWorkErrorMsg:String = ""
+    let placholderImage = UIImage(systemName: "photo.on.rectangle.angled")
     
     var body: some View {
-        Image(uiImage: self.remoteImage ?? placholderImage!)
-            .resizable()
-            .onAppear(perform: fetchRemoteImage)
+        VStack(spacing: 30) {
+            if (self.remoteImage != nil) {
+                Image(uiImage: self.remoteImage ?? placholderImage!)
+                    .resizable()
+                    .frame(height: 200)
+                    .onAppear(perform: fetchRemoteImage)
+                
+                Text("备注：本图片是采用URLSession从网络下载")
+                    .font(.footnote)
+            } else {
+                Image(systemName: "photo.on.rectangle.angled")
+                
+                if netWorkErrorMsg != "" {
+                    Text(netWorkErrorMsg)
+                        .font(.body)
+                        .foregroundColor(.gray)
+                }
+            }
+        }
+        .onAppear(perform: fetchRemoteImage)
     }
     
     func fetchRemoteImage() {
-        guard let url = URL(string: "https://www.baidu.com/img/PCtm_d9c8750bed0b3c7d089fa7d55720d6cf.png") else {return}
-        URLSession.shared.dataTask(with: url) { (data, response, error) in
+        guard let url = URL(string: "https://www.baidu.com/img/PCtm_d9c8750bed0b3c7d089fa7d55720d6cf.png2") else {return}
+        
+        let config = URLSessionConfiguration.default
+        config.allowsExpensiveNetworkAccess = false
+        config.allowsConstrainedNetworkAccess = false
+        config.timeoutIntervalForRequest = 20
+        config.timeoutIntervalForResource = 20
+        config.waitsForConnectivity = true
+        let session = URLSession(configuration: config)
+        
+        session.dataTask(with: url) { (data, response, error) in
             if error != nil {
+                if (error as? URLError)?.code == .timedOut {
+                    netWorkErrorMsg = "网络请求超时，请检查网络"
+                }
+                if (error as? URLError)?.code == .badURL {
+                    netWorkErrorMsg = "错误的URL"
+                }
                 return
             }
+            guard let response = response as? HTTPURLResponse, (200 ..< 530) ~= response.statusCode else {
+                netWorkErrorMsg = "Error: HTTP请求失败"
+                return
+            }
+            let statusCode = response.statusCode
+            if statusCode != 200 {
+                netWorkErrorMsg = "Error: HTTP请求失败, HTTP状态码: \(statusCode)"
+                return
+            }
+            
             if let image = UIImage(data: data!) {
                 self.remoteImage = image
             } else {
@@ -81,14 +123,20 @@ struct exampleNetWorkImage1: View {
     }
 }
 
-struct exampleNetWorkImage2: View {
+@available (iOS 15.0, *)
+struct exampleNetWorkImageUseAsyncImage: View {
     var body: some View {
-        AsyncImage(url: URL(string: "https://ydlunacommon-cdn.nosdn.127.net/abc2e00ba59ece4101a89a01cce818da.png")) { image in
-            image.resizable()
-        } placeholder: {
-            ProgressView()
+        VStack {
+            AsyncImage(url: URL(string: "https://ydlunacommon-cdn.nosdn.127.net/abc2e00ba59ece4101a89a01cce818da.png")) { image in
+                image.resizable()
+            } placeholder: {
+                ProgressView()
+            }
+            .frame(width: 100, height: 100)
+            
+            Text("备注：本图片是采用AsyncImage从网络下载, AsyncImage适用于iOS 15.0+")
+                .font(.footnote)
         }
-        .frame(width: 50, height: 50)
     }
 }
 
