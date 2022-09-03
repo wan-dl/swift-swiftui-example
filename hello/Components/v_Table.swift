95a83703a46eebe8df0bba361db13e3a20783a6a
xiaohutu <shanxi.1991@gmail.com>
2022-09-03 22:05:44 +0800

    update Table Example

--- hello/Components/v_Table.swift
+++ hello/Components/v_Table.swift
@@ -7,28 +7,37 @@
 
 import SwiftUI
 
+struct Person: Identifiable {
+    let givenName: String
+    let familyName: String
+    let emailAddress: String
+    let id = UUID()
+}
+private var people = [
+    Person(givenName: "Juan", familyName: "Chavez", emailAddress: "juanchavez@icloud.com"),
+    Person(givenName: "Mei", familyName: "Chen", emailAddress: "meichen@icloud.com"),
+    Person(givenName: "Tom", familyName: "Clark", emailAddress: "tomclark@icloud.com"),
+    Person(givenName: "Gita", familyName: "Kumar", emailAddress: "gitakumar@icloud.com")
+]
+
 struct v_Table: View {
-    struct Person: Identifiable {
-        let givenName: String
-        let familyName: String
-        let emailAddress: String
-        let id = UUID()
-    }
-    private var people = [
-        Person(givenName: "Juan", familyName: "Chavez", emailAddress: "juanchavez@icloud.com"),
-        Person(givenName: "Mei", familyName: "Chen", emailAddress: "meichen@icloud.com"),
-        Person(givenName: "Tom", familyName: "Clark", emailAddress: "tomclark@icloud.com"),
-        Person(givenName: "Gita", familyName: "Kumar", emailAddress: "gitakumar@icloud.com")
-    ]
     
-//    @available(iOS 16.0)
     var body: some View {
-        Text("")
-//        Table(people) {
-//            TableColumn("Given Name", value: \.givenName)
-//            TableColumn("Family Name", value: \.familyName)
-//            TableColumn("E-Mail Address", value: \.emailAddress)
-//        }
+        VStack {
+            
+            Text("说明: table，仅适用于iOS 16.0+")
+                .font(.caption)
+                .foregroundColor(.gray)
+                .frame(height: 80)
+            
+            if #available(iOS 16.0, *) {
+                Table(people) {
+                    TableColumn("Given", value: \.givenName)
+                    TableColumn("Family", value: \.familyName)
+                    TableColumn("E-Mail", value: \.emailAddress)
+                }
+            }
+        }
     }
 }
 
