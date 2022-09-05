e19cd3eaa381b576fac58409b479064aebc3750c
xiaohutu <shanxi.1991@gmail.com>
2022-09-05 22:20:10 +0800

    update Grid.swift

--- hello/Components/v_Grid.swift
+++ hello/Components/v_Grid.swift
@@ -10,35 +10,35 @@ import SwiftUI
 struct v_Grid: View {
     
     var body: some View {
+        Text("SwiftUI Gird 仅支持iOS 16.0")
+            .font(.body)
+        
         if #available(iOS 16.0, *) {
-//            ShowGrid
-            Text("Gird 仅支持iOS 16.0, 且真机运行有Bug")
-                .font(.body)
-        } else {
-            Text("Gird 仅支持iOS 16.0")
-                .font(.body)
+            ShowGrid
         }
     }
     
-//    @available(iOS 16.0, *)
-//    var ShowGrid: some View {
-//        Grid(alignment: .bottom, horizontalSpacing: 1, verticalSpacing: 1) {
-//            GridRow {
-//                Text("Row 1")
-//                ForEach(0..<2) { _ in Color.red }
-//            }
-//            GridRow {
-//                Text("Row 2")
-//                ForEach(0..<5) { _ in Color.green }
-//            }
-//            GridRow {
-//                Text("Row 3")
-//                ForEach(0..<4) { _ in Color.blue }
-//            }
-//        }
-//        .frame(height: 200)
-//        .padding()
-//    }
+    @available(iOS 16.0, *)
+    var ShowGrid: some View {
+        Grid(alignment: .bottom, horizontalSpacing: 1, verticalSpacing: 1) {
+            GridRow {
+                Text("Row 1")
+                ForEach(0..<2) { _ in Color.red }
+            }
+            GridRow {
+                Text("Row 2")
+                ForEach(0..<5) { _ in Color.green }
+            }
+            GridRow {
+                Text("Row 3")
+                ForEach(0..<4) { _ in Color.blue }
+            }
+        }
+        .frame(height: 200)
+        .padding()
+        .navigationTitle("Grid")
+        .navigationBarTitleDisplayMode(.inline)
+    }
 }
 
 struct v_Grid_Previews: PreviewProvider {
