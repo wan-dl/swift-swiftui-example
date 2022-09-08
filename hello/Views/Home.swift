2ae58756fd22a9a0d2bb0c4c09b33dfe34c20d36
xiaohutu <shanxi.1991@gmail.com>
2022-09-08 09:41:29 +0800

    update home

--- hello/Views/Home.swift
+++ hello/Views/Home.swift
@@ -1,77 +1,120 @@
 //
-//  p_home.swift
+//  Home.swift
 //  HelloSwift
 //
-//  Created by 1 on 7/30/22.
+//  Created by hx on 9/7/22.
 //
 
 import SwiftUI
 
+struct elment: Identifiable {
+    let id = UUID()
+    var desc: String
+    var icon: String
+}
+
+var startList: [elment] = [
+    elment(desc: "简介", icon: ""),
+    elment(desc: "新建项目", icon: ""),
+    elment(desc: "申请\n开发者账号", icon: ""),
+    elment(desc: "JavaScript", icon: "")
+]
+
+var tutorialList: [elment] = [
+    elment(desc: "Swift语言\n官方教程中文版", icon: "icon_swift"),
+    elment(desc: "SwiftUI\n构建用户界面", icon: "icon_swiftui"),
+    elment(desc: "iOS\nFramework", icon: "icon_sdk")
+]
+
 struct Home: View {
     var body: some View {
-        ScrollView() {
-            card            
-//            Color.clear.frame(height: 1000)
+        ScrollView {
+            VStack(alignment: .leading) {
+                Group {
+                    Text("开始")
+                        .font(.title)
+                    startView
+                }
+                
+                Group {
+                    Text("进阶")
+                        .font(.title2)
+                    tutorialView
+                }
+                .offset(y: 40)
+                
+                Spacer()
+            }
+            .padding()
+            .frame(maxWidth: .infinity)
         }
-        .safeAreaInset(edge: .top, content: {
-            Color.clear.frame(height: 70)
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .background(.gray.opacity(0.1))
+        .navigationTitle("Swift")
+        .navigationBarTitleDisplayMode(.inline)
+        .safeAreaInset(edge: .bottom, content: {
+            Color.clear.frame(height: 44)
         })
-        .overlay(
-            NavigationBar(title: "Hello")
-        )
-        .padding(.horizontal, -10)
-//        .navigationTitle("Hello")
-//        .navigationBarTitleDisplayMode(.inline)
-        .navigationBarHidden(true)
     }
     
-    var card: some View {
-        VStack(alignment: .leading, spacing: 8.0) {
-            Spacer()
-            Image("Logo2")
-                .resizable(resizingMode: .stretch)
-                .aspectRatio(contentMode: .fit)
-                .frame(width: 36,height: 36)
-                .cornerRadius(20)
-            Text("SwiftUI For iOS")
-                .font(.largeTitle)
-                .fontWeight(.bold)
-                .foregroundStyle(
-                    .linearGradient(
-                        colors: [.primary, .primary.opacity(0.5)],
-                        startPoint: .topLeading,
-                        endPoint: .bottomLeading
-                    )
-                )
-                
-            Text("使用更少的代码创建更好的应用")
-                .font(.footnote)
-                .foregroundColor(.secondary)
-                .fontWeight(.semibold)
-            Text("SwiftUI是一种使用Swift语言在苹果设备上构建用户界面的创新且简单的方式...")
-                .font(.footnote)
-                .foregroundColor(.secondary)
-                .lineLimit(2)
-                .offset(y: 10)
+    var startView: some View {
+        ScrollView(.horizontal, showsIndicators: false) {
+            HStack {
+                ForEach(startList) { item in
+                    NavigationLink(destination: {
+                        API()
+                    }, label: {
+                        VStack(alignment: .center){
+                            Image(item.icon)
+                                .resizable(resizingMode: .stretch)
+                                .aspectRatio(contentMode: .fit)
+                                .frame(maxWidth: .infinity)
+                                .frame(width: 28, height: 28)
+                                .offset(y: 10)
+                            Text(item.desc)
+                                .font(.subheadline)
+                                .foregroundColor(.black)
+                                .frame(height: 50)
+                                .frame(maxWidth:.infinity)
+                                .padding()
+                        }
+                        .frame(width: 110, height: 110, alignment: .bottomLeading)
+                        .background(.white)
+                        .cornerRadius(10)
+                    })
+                }
+            }
         }
-        .padding(.all, 20.0)
-        .padding(.vertical, 20)
-        .frame(height: 350.0)
-        .background(.ultraThinMaterial)
-        .cornerRadius(30.0)
-        .shadow(radius: 10, x: 0, y: 10)
-        .padding(.horizontal, 20)
-        .background(
-            Image("Blob1")
-                .offset(x: 250, y: -100)
-        )
-        .overlay(
-            Image("Illustration")
-                .resizable()
-                .aspectRatio(contentMode: .fit)
-                .frame(height: 230)
-                .offset(x: 32, y: -80)
-        )
+    }
+    
+    var tutorialView: some View {
+        ScrollView(.horizontal, showsIndicators: false) {
+            HStack() {
+                ForEach(tutorialList) { item in
+                    NavigationLink(destination: {
+                        API()
+                    }, label: {
+                        VStack(alignment: .center){
+                            Image(item.icon)
+                                .resizable(resizingMode: .stretch)
+                                .aspectRatio(contentMode: .fit)
+                                .frame(maxWidth: .infinity)
+                                .frame(width: 64, height: 64)
+                                .offset(y: 10)
+                            Text(item.desc)
+                                .font(.subheadline)
+                                .foregroundColor(.black)
+                                .frame(height: 50)
+                                .frame(maxWidth:.infinity)
+                                .padding()
+                        }
+                        .frame(width: 150, height: 180, alignment: .bottomLeading)
+                        .background(.white)
+                        .cornerRadius(10)
+                    })
+                }
+            }
+        }
     }
 }
 
