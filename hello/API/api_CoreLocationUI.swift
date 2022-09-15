af5cb444ea912077a74e96828b74ac4559136f6c
xiaohutu <shanxi.1991@gmail.com>
2022-09-15 09:06:04 +0800

    优化 CoreLocationUI页面

--- hello/API/api_CoreLocationUI.swift
+++ hello/API/api_CoreLocationUI.swift
@@ -10,17 +10,31 @@ import CoreLocation
 import CoreLocationUI
 import MapKit
 
+// 地图显示，默认经纬度
+private struct testRegion {
+    static let latitude = 30.259324
+    static let longitude = 120.130203
+}
+
+private struct Span {
+    static let delta = 0.1
+}
+
 final class LocationManager: NSObject, ObservableObject, CLLocationManagerDelegate {
     private let locationManager = CLLocationManager()
     
     @Published var location: CLLocationCoordinate2D?
-    @Published var region = MKCoordinateRegion(
-        center: CLLocationCoordinate2D(latitude: 42.0422448, longitude: -102.0079053),
-        span: MKCoordinateSpan(latitudeDelta: 0.02, longitudeDelta: 0.02)
+    
+    @Published var region: MKCoordinateRegion = .init(
+        center: CLLocationCoordinate2D(latitude: testRegion.latitude, longitude: 120.130203),
+        span: MKCoordinateSpan(latitudeDelta: Span.delta, longitudeDelta: Span.delta)
     )
 
     override init() {
         super.init()
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
         locationManager.delegate = self
     }
 
@@ -29,14 +43,14 @@ final class LocationManager: NSObject, ObservableObject, CLLocationManagerDelega
     }
 
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-        print(locations)
         guard let location = locations.first else { return }
+        print("Location :: \(location)")
         
         DispatchQueue.main.async {
             self.location = location.coordinate
-            self.region = MKCoordinateRegion(
+            self.region = .init(
                 center: location.coordinate,
-                span: MKCoordinateSpan(latitudeDelta: 0.02, longitudeDelta: 0.02)
+                span: MKCoordinateSpan(latitudeDelta: 0.1, longitudeDelta: 0.1)
             )
         }
     }
@@ -50,34 +64,33 @@ final class LocationManager: NSObject, ObservableObject, CLLocationManagerDelega
 struct api_CoreLocationUI: View {
     @StateObject var locationManager = LocationManager()
 
-        var body: some View {
-            ZStack(alignment: .bottom) {
-                Map(coordinateRegion: $locationManager.region, showsUserLocation: true)
-                    .edgesIgnoringSafeArea(.all)
-                
-                VStack {
-                    if let location = locationManager.location {
-                        Text("当前经纬度: \(location.latitude), \(location.longitude)")
-                            .font(.callout)
-                            .foregroundColor(.white)
-                            .padding()
-                            .background(.gray)
-                            .clipShape(RoundedRectangle(cornerRadius: 10))
-                    }
-
-                    Spacer()
-                    LocationButton {
-                        locationManager.requestLocation()
-                    }
-                    .frame(width: 200, height: 40)
-                    .cornerRadius(30)
-                    .symbolVariant(.fill)
-                    .foregroundColor(.white)
+    var body: some View {
+        ZStack(alignment: .bottom) {
+            Map(coordinateRegion: $locationManager.region, showsUserLocation: true)
+                .edgesIgnoringSafeArea(.all)
+            
+            VStack {
+                if let location = locationManager.location {
+                    Text("当前经纬度: \(location.latitude), \(location.longitude)")
+                        .font(.callout)
+                        .foregroundColor(.white)
+                        .padding()
+                        .background(.gray)
+                        .clipShape(RoundedRectangle(cornerRadius: 10))
                 }
-                .padding()
+                
+                LocationButton(.currentLocation) {
+                    locationManager.requestLocation()
+                }
+                .symbolVariant(.fill)
+                .labelStyle(.titleAndIcon)
+                .foregroundColor(.white)
             }
-            .navigationTitle("CoreLocationUI")
+            .padding()
         }
+        .navigationTitle("CoreLocationUI")
+        .modifier(navBarViewCodeAndDocs(pageType: "API",pageID: "CoreLocationUI"))
+    }
 }
 
 struct api_CoreLocationUI_Previews: PreviewProvider {
