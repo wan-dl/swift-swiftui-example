7c391af007ab27dabb1e0f3be27f3494c4c6f5ed
xiaohutu <shanxi.1991@gmail.com>
2022-09-17 14:23:04 +0800

    update api_clipboard.swift

--- hello/API/api_clipboard.swift
+++ hello/API/api_clipboard.swift
@@ -7,23 +7,13 @@
 
 import SwiftUI
 
-//enum FocusedField:Hashable{
-//   case name,password
-//}
-
 struct api_clipboard: View {
     @State var inputText:String = ""
     @FocusState var isFocused: Bool
     
     var body: some View {
-        ScrollView() {
-            
-            Text("说明：本页面功能主要用到了： UIPasteboard.general ")
-                .font(.caption)
-                .foregroundColor(.gray)
-                .frame(height: 80)
-            
-            VStack {
+        Form {
+            Section(header: Text("剪切板功能，主要用到了 UIPasteboard.general").textCase(.none)) {
                 TextEditor(text: $inputText)
                     .frame(height: 200)
                     .cornerRadius(10)
@@ -33,28 +23,22 @@ struct api_clipboard: View {
                             self.isFocused = true
                         }
                     }
-                
-                Button("将输入框内容写入剪贴板", action: {
-                    UIPasteboard.general.string = self.inputText
-                })
-                .buttonStyle(PrimaryBtnStyle())
-                
-                Button("读取剪切板到输入框", action: {
-                    if UIPasteboard.general.hasStrings {
-                        self.inputText = UIPasteboard.general.string ?? ""
-                    }
-                })
-                .buttonStyle(PrimaryBtnStyle())
             }
             
+            Button("将输入框内容写入剪贴板", action: {
+                UIPasteboard.general.string = self.inputText
+            })
+            
+            Button("读取剪切板到输入框", action: {
+                if UIPasteboard.general.hasStrings {
+                    self.inputText = UIPasteboard.general.string ?? ""
+                }
+            })
         }
-        .padding(.horizontal, 10)
-        .background(.gray.opacity(0.1))
         .navigationTitle("clipboard")
         .modifier(navBarViewCodeAndDocs(pageType: "API",pageID: "clipboard"))
     }
 }
-
 
 struct api_clipboard_Previews: PreviewProvider {
     static var previews: some View {
