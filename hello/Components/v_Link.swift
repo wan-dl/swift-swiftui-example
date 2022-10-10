8740eaf2f406d40f0a6d73ad32efd4ab0267afb1
xiaohutu <shanxi.1991@gmail.com>
2022-10-10 09:56:50 +0800

    update v_Link.swift

--- hello/Components/v_Link.swift
+++ hello/Components/v_Link.swift
@@ -12,12 +12,12 @@ struct v_Link: View {
     var body: some View {
         
         VStack(spacing: 20) {
-            Link("点击apple.com在浏览器打开",
+            Link("Link示例：点击apple.com在浏览器打开",
                  destination: URL(string: "https://www.apple.com")!)
             
             Divider()
             
-            Link("Visit Our Site", destination: URL(string: "https://www.example.com")!)
+            Link("Link示例：Visit Our Site", destination: URL(string: "https://www.example.com")!)
                 .environment(\.openURL, OpenURLAction { url in
                     print("您点击的URL是： \(url)")
                     return .handled
@@ -25,11 +25,11 @@ struct v_Link: View {
             
             Divider()
             
-//            if #available(iOS 16.0, *) {
-//                ShareLink(item: URL(string: "https://developer.apple.com/xcode/swiftui/")!) {
-//                    Label("ShareLink示例", image: "square.and.arrow.up")
-//                }
-//            }
+            if #available(iOS 16.0, *) {
+                ShareLink(item: URL(string: "https://developer.apple.com/xcode/swiftui/")!) {
+                    Label("ShareLink示例", image: "square.and.arrow.up")
+                }
+            }
         }
         .navigationTitle("Link")
         .navigationBarTitleDisplayMode(.inline)
